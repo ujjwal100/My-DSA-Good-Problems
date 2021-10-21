ee57b9a11b03b824c587d37aaf3c0bc76a1f0700
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2021-10-21 13:55:55 +0530

    Upload code

---
+++ Arrays/First Missing Positive.java
@@ -0,0 +1,27 @@
+class Solution {
+  // Q (LC - 41): Find MEX of unsorted array (can have multiple repeating instances of a number) in O(N) T, O(1) S
+  
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            if (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                //swap
+                int temp = nums[nums[i] - 1] ;
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+        
+        i = 0;
+        while (i < n) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+            i++;
+        }
+        return n + 1;
+    }
+}
