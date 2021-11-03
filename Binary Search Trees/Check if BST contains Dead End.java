39bed187261227b499e4eac518ab6718401751ab
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2021-11-03 14:42:03 +0530

    Upload Code

---
+++ Binary Search Trees/Check if BST contains Dead End.java
@@ -0,0 +1,22 @@
+class GFG
+{
+  // Problem : <a href="https://practice.geeksforgeeks.org/problems/check-whether-bst-contains-dead-end/1#">Link</a>   
+  // O(N) Time + O(H) Space  
+  public static boolean isDeadEnd(Node root)
+    {
+        //Add your code here.
+        return isDeadEndHelper(root, 0, Integer.MAX_VALUE);
+        
+    }
+    private static boolean isDeadEndHelper(Node root, int pred, int succ) {
+        if (root == null) {
+            return false;
+        }
+        
+        if (root.left == null && root.right == null && pred == root.data - 1 && succ == root.data + 1) {
+            return true;
+        } else {
+            return isDeadEndHelper(root.left, pred, root.data) || isDeadEndHelper(root.right, root.data, succ);
+        }
+    }
+}
