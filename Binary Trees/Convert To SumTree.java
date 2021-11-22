fe9a4fe0fe8623143978893e2e5b014d77c2bb9a
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2021-11-22 12:04:12 +0530

    Upload code

---
+++ Binary Trees/Convert To SumTree.java
@@ -0,0 +1,25 @@
+class Solution{
+    // O(N) Time + O(H) Space
+    // Problem link : https://practice.geeksforgeeks.org/problems/transform-to-sum-tree/1
+  
+    public void toSumTree(Node root){
+         //add code here.
+         
+         toSumTreeUtil(root);
+         }
+         
+     private int toSumTreeUtil(Node root) {
+         if (root == null) {
+             return 0;
+         }
+         
+         int subtreeSum = toSumTreeUtil(root.left) + toSumTreeUtil(root.right);
+         
+         int ans = root.data + subtreeSum;
+         
+         root.data = subtreeSum;
+         
+         return ans;
+     }
+     
+}
