d8dbd2bcb6ed8fa2388683a4f95cccc8fcf434d4
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2021-11-26 13:34:01 +0530

    Update

--- Arrays/First Missing Positive.java
+++ Arrays/First Missing Positive.java
@@ -25,4 +25,34 @@ class Solution {
         }
         return n + 1;
     }
+  
+  /*
+   * ALTERNATE SOLUTION : Mark Index Negative
+   * O(N) Time (3 Iterations) + O(1) Space
+   */
+  public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                nums[i] = 0;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0 && Math.abs(nums[i]) <= n) {
+                if (nums[Math.abs(nums[i]) - 1] > 0)
+                    nums[Math.abs(nums[i]) - 1] *= (-1);
+                else if (nums[Math.abs(nums[i]) - 1] == 0)
+                    nums[Math.abs(nums[i]) - 1] = -(n + 1); 
+            } 
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                return i + 1;
+            }
+        }
+        
+        return n + 1;
+    }
 }
