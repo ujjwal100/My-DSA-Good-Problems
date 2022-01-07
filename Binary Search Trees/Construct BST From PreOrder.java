9b18e96d9ff33ae54d07c527066124ebdbd6a80c
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2022-01-07 14:59:32 +0530

    Update With More Optimal code

--- Binary Search Trees/Construct BST From PreOrder.java
+++ Binary Search Trees/Construct BST From PreOrder.java
@@ -2,26 +2,22 @@ class Solution {
 // https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
 // O(N) Time O(H) Space Solution
 
-    static int i;
+    static int index;
     public TreeNode bstFromPreorder(int[] preorder) {
-        i = 0;
-        return buildBST(preorder, null);
+        index = 0;
+        return getBST(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
-    private TreeNode buildBST(int[] preorder, TreeNode ancestor) {
-        if (i >= preorder.length) {
+    
+    private TreeNode getBST(int[] preorder, int low, int high) {
+        if (index >= preorder.length || preorder[index] < low || preorder[index] > high) {
             return null;
+        } else {
+            TreeNode root = new TreeNode(preorder[index++]);
+            
+            root.left = getBST(preorder, low, root.val - 1);
+            root.right = getBST(preorder, root.val + 1, high);
+            
+            return root;
         }
-        TreeNode root = new TreeNode(preorder[i]);
-        i++;
-        
-        if (i < preorder.length && preorder[i] < root.val) {
-            root.left = buildBST(preorder, root);
-        }
-        
-        if (i < preorder.length && (ancestor == null || preorder[i] < ancestor.val) ) {
-            root.right = buildBST(preorder, ancestor);
-        }
-        
-        return root;
     }
 }
