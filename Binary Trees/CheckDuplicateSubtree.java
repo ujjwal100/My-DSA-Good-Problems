440db8cdab5e91b440fd7ff2f885ab8bfee3d47e
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2022-01-11 10:05:38 +0530

    Update with more optimal code

--- Binary Trees/CheckDuplicateSubtree.java
+++ Binary Trees/CheckDuplicateSubtree.java
@@ -1,51 +1,44 @@
 class Solution {
-  /**
+ /**
   * Problem Link : https://practice.geeksforgeeks.org/problems/duplicate-subtree-in-binary-tree/1#
   * Time Complexity of My solution : O(N)
   * Space Complexity : O(N)
   * (Google Interview Problem)
   */
-    private static class HashedString {
-        String hashVal;
-        HashedString() {
-            hashVal = "";
-        }
+    static String inorderTraversalForSubtree;
+    Set<String> subtrees;
+    int dupSub(Node root) {
+        // code here 
+        subtrees = new HashSet<String>();
+        return (hasDuplicateSubtree(root) == true? 1 : 0);
     }
     
-    int checkDuplicateSubTree(Node root) { 
-        Map<String, Integer> map = new HashMap<>();
-        
-        if (hashSubTrees(root, new HashedString(), map)) {
-            return 1;
-        }
-        
-        return 0;
-    }
-    private boolean hashSubTrees(Node root, HashedString str, Map<String, Integer> map) {
+    private boolean hasDuplicateSubtree(Node root) {
         if (root == null) {
-            str.hashVal = "$";
+            inorderTraversalForSubtree = "";
             return false;
-        }
-        
-        HashedString leftSubTree = new HashedString();
-        HashedString rightSubTree = new HashedString();
-        
-        if (hashSubTrees(root.left, leftSubTree, map) || hashSubTrees(root.right, rightSubTree, map)) {
-            return true;
-        }
-        
-        
-        str.hashVal = "$" + root.data + "$" + leftSubTree.hashVal + rightSubTree.hashVal;
-        
-        if (root.left != null || root.right != null) { // If Non Leaf (subtree size > 1)
-            if (map.containsKey(str.hashVal)) {
+        } else {
+            if (hasDuplicateSubtree(root.left)) {
                 return true;
-            } else {
-                map.put(str.hashVal, 1);
+            }
+            String leftSubtree = inorderTraversalForSubtree;
+            
+            if (hasDuplicateSubtree(root.right)) {
+                return true;
             }
             
-        } 
-        
-        return false;
+            String rightSubtree = inorderTraversalForSubtree;
+            
+            inorderTraversalForSubtree = leftSubtree + " " + root.data + " " + rightSubtree;
+            
+            if (subtrees.contains(inorderTraversalForSubtree) && (leftSubtree.length() > 0 || rightSubtree.length() > 0)) {
+              // left | right subtree is non-null
+                return true;
+            } else {
+                subtrees.add(inorderTraversalForSubtree);
+                return false;
+            }
+             
+        }
     }
 }
