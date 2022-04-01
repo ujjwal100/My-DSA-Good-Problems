5723507cdddcab2442554de346616c5dc91ad4e7
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2022-04-01 22:37:03 +0530

    Upload

--- Heaps/Min Cost To Connect Sticks.java
+++ Heaps/Min Cost To Connect Sticks.java
@@ -1,4 +1,5 @@
 // O(NlogN) Time + O(N) Space 
+// Link : https://github.com/ujjwal100/LC-Premium/blob/main/Min%20Cost%20To%20Connect%20Sticks.png
 class Solution {
     public int connectSticks(int[] sticks) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
