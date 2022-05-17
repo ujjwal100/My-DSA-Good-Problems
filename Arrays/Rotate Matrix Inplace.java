8c34e5266737c235e6aa2413c17657c4d8e5e52c
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2022-05-17 12:40:25 +0530

    Update

--- Arrays/Rotate Matrix Inplace.java
+++ Arrays/Rotate Matrix Inplace.java
@@ -1,52 +1,11 @@
 // Problem : https://www.interviewbit.com/problems/rotate-matrix/
-// Observation : total shift i + j = dim - 1
-// Observation : ORDER:  [j++,i++] -> [i++,j--] -> [j--,i--] -> [i--,j++]
 // O(N^2) Time O(1) space
 public class Solution {
 	public void rotate(ArrayList<ArrayList<Integer>> a) {
-        int dimension = a.size();
-        int startRowCol = 0;
-        int endRowCol = a.size() - 1;
-
-        while (dimension > 0) {
-            int i = startRowCol, j = startRowCol;
-
-            int counter = 1;
-            while (counter++ < dimension) {
-                
-                int curr = a.get(i).get(j);
-
-                i += (dimension - 1) - (endRowCol - j); // total shift = dim - 1
-                j = endRowCol;
-
-                int next = a.get(i).get(j);
-                a.get(i).set(j, curr);
-                curr = next;
-
-                j -= (dimension - 1) - (endRowCol - i);
-                i = endRowCol;
-
-                next = a.get(i).get(j);
-                a.get(i).set(j, curr);
-                curr = next;
-
-                i -= (dimension - 1) - (j - startRowCol);
-                j = startRowCol;
-
-                next = a.get(i).get(j);
-                a.get(i).set(j, curr);
-                curr = next;
-
-                j += (dimension - 1) - (i - startRowCol);
-                i = startRowCol;
-                
-                a.get(i).set(j, curr);
-                j++;
-            }
-
-            startRowCol++;
-            endRowCol--;
-            dimension -= 2;
-        }
+		int n=matrix.size() , m=matrix[0].size();
+		reverse(matrix.begin(),matrix.end());
+		for(int i=0;i<n;i++)
+			for(int j=i+1;j<m;j++)
+				swap(matrix[i][j],matrix[j][i]);
 	}
 }
