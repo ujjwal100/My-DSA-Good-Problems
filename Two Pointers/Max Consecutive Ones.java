66a6b86aa820f8d89fbe94e3b24a480601b08b1e
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2022-06-13 13:14:31 +0530

    Add

--- Two Pointers/Max Consecutive Ones.java
+++ Two Pointers/Max Consecutive Ones.java
@@ -6,21 +6,22 @@ SC : O(1)
 
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int left = 0, right;
-        for (right = 0; right < nums.length; right++) {
-            // If we included a zero in the window we reduce the value of k.
-            // Since k is the maximum zeros allowed in a window.
-            if (nums[right] == 0) {
-                k--;
+        int l = 0, r = 0, zeroes = nums[r]==0?1:0, ans=0;    
+        while (r < nums.length) {
+            if (zeroes <= k) {
+                ans = Math.max(ans, r - l + 1);
+                r++;
+                if (r < nums.length && nums[r] == 0) {
+                    zeroes++;
+                }
+            } else {
+                l++;
+                if (nums[l-1] == 0) {
+                    zeroes--;
+                }
             }
-            // A negative k denotes we have consumed all allowed flips and window has
-            // more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
-            if (k < 0) {
-                // If the left element to be thrown out is zero we increase k.
-                k += 1 - nums[left];
-                left++;
-            }
-        }     
-        return right - left;
+        }
+        
+        return ans;
     }
 }
