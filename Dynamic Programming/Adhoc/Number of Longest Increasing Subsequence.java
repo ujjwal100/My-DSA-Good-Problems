305f3d53adb8772e9136597816dd94a2276351a3
Ujjwal Chakraborty <ujjwal.chakraborty.01@gmail.com>
2022-06-21 13:53:12 +0530

    Update

--- Dynamic Programming/Adhoc/Number of Longest Increasing Subsequence.java
+++ Dynamic Programming/Adhoc/Number of Longest Increasing Subsequence.java
@@ -7,28 +7,31 @@ class Solution {
     public int findNumberOfLIS(int[] nums) {
        int n = nums.length;
         int[] lis = new int[n];
-        int[][] numLis = new int[n+1][n];
+        int[] numLis = new int[n];
         
         int maxLis = 0, numMaxLis = 0;
         
         for (int i = 0; i < n; i++) {
             lis[i] = 1;
-            numLis[1][i] += 1;
+            numLis[i] += 1;
             
             for (int j = i - 1; j >= 0; j--) {
                 if (nums[i] > nums[j]) {
-                    numLis[lis[j] + 1][i] += numLis[lis[j]][j];
                     if (lis[i] < lis[j] + 1) {
                         lis[i] = lis[j] + 1;
+                        numLis[i] = numLis[j];
+                    } else if (lis[i] == lis[j] + 1) {
+                        numLis[i] += numLis[j];
                     }
                 }
             }
             
+            
             if (lis[i] == maxLis) {
-                numMaxLis += numLis[lis[i]][i];
+                numMaxLis += numLis[i];
             } else if (lis[i] > maxLis) {
                 maxLis = lis[i];
-                numMaxLis = numLis[lis[i]][i];
+                numMaxLis = numLis[i];
             }
         }
         
